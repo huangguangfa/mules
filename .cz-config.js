860e0036c39c4bca8c81d0bada84c570480bba63
gf <1454556135@qq.com>
2022-03-25 17:41:00 +0800

    feat(测试11111): feat(组件): feat(组件): docs(组件): 测试
    1

--- .cz-config.js
+++ .cz-config.js
@@ -11,7 +11,7 @@ module.exports = {
         { value: "revert", name: "revert 🌝:     版本回滚" },
         { value: "update", name: "update ⬆:    第三方库升级 " }
     ],
-    scopes: [{ name: '组件' }, { name: '样式' }, { name: '文档更改' }, { name: '其它变更' }],
+    scopes: [{ name: '组件' }, { name: '样式' }, { name: '文档更改' },{ name: '测试11111' }, { name: '其它变更' }],
     allowTicketNumber: false,
     isTicketNumberRequired: false,
     ticketNumberPrefix: 'TICKET-',
