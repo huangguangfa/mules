fee84014ef8ad50874546268780e5e6ac48a5203
hgf <1454556135@qq.com>
2022-04-22 01:49:15 +0800

    feat(样式): less => scss
    -

--- packages/sten-themes/gulpfile.js
+++ packages/sten-themes/gulpfile.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const { series, src, dest } = require('gulp');
-const less = require('gulp-less');
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 function compile() {
-    return src('./src/*.less')
-        .pipe(less({
-            // paths: [ path.join(__dirname, 'less', 'includes')]
-        }))
+    return src('./src/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             cascade: false
         }))
