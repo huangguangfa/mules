8aea5ddcd7944001511743394ca5bb2a8e9e3489
hgf <1454556135@qq.com>
2022-04-30 23:29:36 +0800

    feat(组件): 添加注释
    - @open-ui/core@0.0.1
    - @open-ui/components@0.0.1
    - @open-ui/icons@0.0.1
    - @open-ui/themes@0.0.1

--- packages/sten-components/src/utils/dom.ts
+++ packages/sten-components/src/utils/dom.ts
@@ -1,5 +1,9 @@
 
-
+/**
+* @overview 设置样式表
+* @author gf
+* @param { v }  
+*/
 export function setStyleSheet(v: string): void {
     let styleSheet = document.styleSheets[0];
     styleSheet.insertRule(v);
