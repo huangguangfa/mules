2eecef3521d693d0a7f7b0d58615bbf28562f40a
hgf <1454556135@qq.com>
2022-05-31 23:02:46 +0800

    feat(组件): feat(组件): feat(npm包): lerna publish
    - @gf-ui/cli@1.0.4

--- packages/cli/comand/init.js
+++ packages/cli/comand/init.js
@@ -16,10 +16,10 @@ module.exports = function (projectName) {
             // 1. clone git项目
             console.log('🚀正在为您创建项目: ' + projectName);
 
-            await clone(`https://github.com/huanggungfa/doc-comment/blob/main/doc/config/plugins.config.ts`, projectName);
+            await clone(`direct:https://github.com/huanggungfa/doc-comment/blob/main/doc/config`, projectName, { clone: true });
             // await clone(`github.com:huanggungfa/${choice}`, projectName);
             console.log(symbols.success, chalk.green(`项目${projectName}创建成功`));
-            console.log(chalk.red('cd ' + projectName + '\nnpm install\nnpm run dev或者npm run start'))
+            console.log(chalk.blue('cd ' + projectName + '\nnpm install\nnpm run dev或者npm run start'))
             // 2. 同步package.json的配置.
             const packageJson = path.join(path.resolve(projectName), 'package.json');
             const repositoryObj = repository ? {
