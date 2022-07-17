d307f0ad28a5f015ef724848db291151509523be
hgf <1454556135@qq.com>
2022-07-17 23:39:42 +0800

    feat(组件): 移除一些不需要的组件
    添加open code

--- docs/.vitepress/vitepress/composables/sidebar.ts
+++ docs/.vitepress/vitepress/composables/sidebar.ts
@@ -1,71 +1,71 @@
-import { computed } from 'vue'
-import { useData, useRoute } from 'vitepress'
-import { ensureStartingSlash, isArray, removeExtention } from '../utils'
-import { useLang } from './lang'
+import { computed } from "vue";
+import { useData, useRoute } from "vitepress";
+import { ensureStartingSlash, isArray, removeExtention } from "../utils";
+import { useLang } from "./lang";
 
 export const useSidebar = () => {
-  const route = useRoute()
-  const { site, page } = useData()
-  const lang = useLang()
+  const route = useRoute();
+  const { site, page } = useData();
+  const lang = useLang();
   if (!page.value) {
     return {
       sidebars: computed(() => []),
       hasSidebar: computed(() => false),
-    }
+    };
   }
   const sidebars = computed(() => {
-    if (page.value.frontmatter.sidebar === false) return []
+    if (page.value.frontmatter.sidebar === false) return [];
     const sidebars = getSidebarConfig(
       site.value.themeConfig.sidebars,
       route.data.relativePath,
       lang.value
-    )
-    return sidebars
-  })
+    );
+    return sidebars;
+  });
 
   return {
     sidebars,
     hasSidebar: computed(() => sidebars.value.length > 0),
-  }
-}
+  };
+};
 
 export function isSideBarConfig(sidebar) {
-  return sidebar === false || sidebar === 'auto' || isArray(sidebar)
+  return sidebar === false || sidebar === "auto" || isArray(sidebar);
 }
 export function isSideBarGroup(item) {
-  return item.children !== undefined
+  return item.children !== undefined;
 }
 export function isSideBarEmpty(sidebar) {
-  return isArray(sidebar) ? sidebar.length === 0 : !sidebar
+  return isArray(sidebar) ? sidebar.length === 0 : !sidebar;
 }
 
 type SidebarItem = {
-  text: string
-  link: string
-}
+  text: string;
+  link: string;
+};
 
-type SidebarConfig = SidebarItem[]
+type SidebarConfig = SidebarItem[];
 
 type Sidebar =
   | {
-      [key: string]: SidebarConfig
+      [key: string]: SidebarConfig;
     }
   | false
-  | 'auto'
+  | "auto";
 
 export function getSidebarConfig(sidebar: Sidebar, path: string, lang: string) {
-  if (sidebar === false || Array.isArray(sidebar) || sidebar === 'auto') {
-    return []
+  if (sidebar === false || Array.isArray(sidebar) || sidebar === "auto") {
+    return [];
   }
 
-  path = ensureStartingSlash(path)
+  path = ensureStartingSlash(path);
   for (const dir in sidebar) {
     // make sure the multi sidebar key starts with slash too
     if (path.startsWith(ensureStartingSlash(`${lang}${dir}`))) {
-      return sidebar[dir][lang]
+      return sidebar[dir][lang];
     }
   }
-  return []
+  return [];
 }
 /**
  * Get flat sidebar links from the sidebar items. This method is useful for
@@ -76,11 +76,11 @@ export function getSidebarConfig(sidebar: Sidebar, path: string, lang: string) {
 export function getFlatSideBarLinks(sidebar) {
   return sidebar.reduce((links, item) => {
     if (item.link) {
-      links.push({ text: item.text, link: removeExtention(item.link) })
+      links.push({ text: item.text, link: removeExtention(item.link) });
     }
     if (isSideBarGroup(item)) {
-      links = [...links, ...getFlatSideBarLinks(item.children)]
+      links = [...links, ...getFlatSideBarLinks(item.children)];
     }
-    return links
-  }, [])
+    return links;
+  }, []);
 }
