c409024cbe1228c3994f39bc71e0a0d9de4b5d7c
hgf <1454556135@qq.com>
2022-07-17 21:26:26 +0800

    feat(组件): g
    添加open code

--- docs/.vitepress/vitepress/index.ts
+++ docs/.vitepress/vitepress/index.ts
@@ -1,28 +1,28 @@
-import 'normalize.css'
+import "normalize.css";
 // import 'element-plus/dist/index.css'
 
 // for dev
 // reset
-import '../../../packages/theme-chalk/src/reset.scss'
-import '../../../packages/theme-chalk/src/index.scss'
+// import '../../../packages/theme-chalk/src/reset.scss'
+// import '../../../packages/theme-chalk/src/index.scss'
 // for dark mode
-import '../../../packages/theme-chalk/src/dark/css-vars.scss'
+// import '../../../packages/theme-chalk/src/dark/css-vars.scss'
 
-import './styles/css-vars.scss'
-import './styles/app.scss'
+import "./styles/css-vars.scss";
+import "./styles/app.scss";
 
-import 'uno.css'
+import "uno.css";
 
-import VPApp from './components/vp-app.vue'
-import VPDemo from './components/vp-demo.vue'
-import IconList from './components/globals/icons.vue'
+import VPApp from "./components/vp-app.vue";
+import VPDemo from "./components/vp-demo.vue";
+import IconList from "./components/globals/icons.vue";
 
-import type { Component } from 'vue'
+import type { Component } from "vue";
 
-export { default as NotFound } from './components/vp-not-found.vue'
+export { default as NotFound } from "./components/vp-not-found.vue";
 
-export default VPApp
+export default VPApp;
 export const globals: [string, Component][] = [
-  ['Demo', VPDemo],
-  ['IconList', IconList],
-]
+  ["Demo", VPDemo],
+  ["IconList", IconList],
+];
