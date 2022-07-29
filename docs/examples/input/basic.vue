63fffa9edc827f2cfb87937403eda6ec44c2ed54
hgf <1454556135@qq.com>
2022-07-29 20:23:16 +0800

    feat(其它变更): lerna => pnpm => monorepo
    添加button

--- docs/examples/input/basic.vue
+++ docs/examples/input/basic.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="reset" style="width: 300px">
-    <gf-input placeholder="请输入姓名"></gf-input>
+    <gf-input placeholder="请输入账号" clearable></gf-input>
+    <gf-input placeholder="请输入密码" type="password" clearable></gf-input>
     <gf-input placeholder="请输入审批内容" type="textarea"></gf-input>
   </div>
 </template>
--- packages/sten-components/src/components/input/gf-input.tsx
+++ packages/sten-components/src/components/input/gf-input.tsx
@@ -4,8 +4,6 @@ import { injectComponents } from "../../utils"
 import { GfIconclear } from "../gf-icon/gf-icon-clear"
 import { GfIconinfo } from "../gf-icon/gf-icon-info"
 import { GfIconsuccessFill } from "../gf-icon/gf-icon-successFill"
-// import { GfAutocomplete } from "./autocomplete"
-
 injectComponents({
   GfIconclear, GfIconinfo, GfIconsuccessFill
 });
@@ -167,6 +165,7 @@ export class GfInput {
       <span>{this.curentValue.length}</span>/{this.maxlength}
     </div>
   }
+
   private getInputInstance = () => {
     return this.type !== 'textarea' ?
       <input
@@ -216,7 +215,6 @@ export class GfInput {
           {Number(this.maxlength) > 0 ? this.getMaxLengthInstance() : ''}
           {this.status ? this.getStatusInstance() : ''}
           <slot name='after'></slot>
-          {/* <gf-autocomplete show={this.autocompleteState}></gf-autocomplete> */}
         </div>
       </Host>
     );
